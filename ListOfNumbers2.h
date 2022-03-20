c230482b8291dd12274e306dc90b4cdb3bcefaea
nicholas2905 <99874235+nicholas2905@users.noreply.github.com>
2022-03-20 03:32:31 +0100

    Update ListOfNumbers2.h

--- ListOfNumbers2.h
+++ ListOfNumbers2.h
@@ -7,26 +7,26 @@ ListOfNumbers::ListOfNumbers()
 {
     number = 0;
     next = NULL;
-}
+};
 ListOfNumbers::ListOfNumbers(int v)
 {
     number = v;
     next = NULL;
-}
+};
 
 void ListOfNumbers::SetValue(int v)
 {
     number = v;
-}
+};
 void ListOfNumbers::SetNext(ListOfNumbers *n)
 {
     next = n;
-}
+};
 // Prints a single item’s number
 void ListOfNumbers::PrintItem()
 {
     cout << "Number = " << number << "      [" << this << " ; " << next << "]" << endl;
-}
+};
 // Prints the whole list
 void ListOfNumbers::PrintList()
 {
@@ -37,12 +37,12 @@ void ListOfNumbers::PrintList()
         cout << current->number << "      [" << current << "]" << endl;
         current = current->next;
     }
-}
+};
 std::ostream & operator << (std::ostream &out, const ListOfNumbers &a)
 {
     out << "List of Numbers Value = " << a.number;
     return out;
-}
+};
 void ListOfNumbers::Add(ListOfNumbers *n)
 {
     ListOfNumbers *current = this;
@@ -51,15 +51,15 @@ void ListOfNumbers::Add(ListOfNumbers *n)
         current = current->next;
     }
     current->SetNext(n);
-}
+};
 
 void ListOfNumbers::Add(int v)
 {
     ListOfNumbers *temp = new ListOfNumbers(v);
 
     Add(temp);
-}
-void ListOfNumbers::AddR(ListOfNumbers *n)
+};
+void ListOfNumbers::AddRPointer(ListOfNumbers *n)
 {
     if (next == NULL)
     {
@@ -67,27 +67,69 @@ void ListOfNumbers::AddR(ListOfNumbers *n)
     }
     else
     {
-        next->AddR(n);
+        next->AddRPointer(n);
     }
-}
+};
 void ListOfNumbers::AddR(int v)
 {
     ListOfNumbers *temp = new ListOfNumbers(v);
 
-    AddR(temp);
-}
-void ListOfNumbers::GetNumber()
+    AddRPointer(temp);
+};
+int ListOfNumbers::GetNumber()
 {
-    int inputted_number;
+    int temp;
     cout << "Please enter a number:\n";
-    cin >> number;
-    SetValue(inputted_number);
-}
-void ListOfNumbers::Find(ListOfNumbers *n)
+    cin >> temp;
+    return temp;
+};
+ListOfNumbers* ListOfNumbers::Find(int find_number)
 {
     ListOfNumbers *current = this;
-    while (current->next == this) 
-        cout << current;
-        //current = current->next;
-    //current->next = (n);
+    if (find_number == number)
+        {
+            return current;
+        }
+    else
+    {
+        while (current -> next != NULL)
+        {
+            current = current -> next;
+            if (find_number == current -> number)
+            {
+                return current;
+            }
+        }
+        return NULL;
+    }
+};
+ListOfNumbers* ListOfNumbers::FindR(int find_number)
+{
+    if (find_number == number)
+        {
+            return this;
+        }
+    else
+    {
+        if (next != NULL)
+        {
+            return next -> FindR(find_number);
+        }
+        else
+        {
+            return NULL;
+        };
+    }
+        
+};
+void ListOfNumbers::NewListOfNumbers()
+{
+    int temp;
+    temp = GetNumber();
+    AddR(temp);
+};
+ListOfNumbers* ListOfNumbers::Delete_Number (int delete_number)
+{
+
+
 };
