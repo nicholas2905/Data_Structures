1ba68ca94ba5629945fb231b5e70eefaf1cd8241
nicholas2905 <99874235+nicholas2905@users.noreply.github.com>
2022-04-03 16:42:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,40 +5,81 @@ using namespace std;
 // Main function
 int main()
 {
-	ListOfNumbers *a,*b,*c;
- 	a = new ListOfNumbers(10);
- 	b = new ListOfNumbers(5);
- 	c = new ListOfNumbers(-24);
+	Node *a,*b,*c;
+    ListOfNumbers * aList;
+ 	a = new Node(10);
+ 	b = new Node(5);
+ 	c = new Node(24);
 
- 	a->SetNext(b);
- 	b->SetNext(c);
- 	
-    cout << "Values : " << endl;
- 	a->PrintItem();
- 	b->PrintItem();
- 	c->PrintItem();
- 	
-    cout << "List a : " << endl;
- 	a->PrintList();
- 	
-    cout << "\nList c : " << endl;
- 	c->PrintList();
+ 	cout << "Test the Sort Method: " << endl;
 
-    cout << "Add method (test) " << endl;
-    a->Add(new ListOfNumbers(45));
-    a->PrintList();
+	a->SetNext(b);
+	b->SetNext(c);
 
-    cout << "AddR method (test) " << endl;
-    a->AddR(new ListOfNumbers(3));
-    a->PrintList();
+	cout << "Values: " << endl;
+	a->PrintItem();
+	b->PrintItem();
+	c->PrintItem();
 
-    cout << "Sort method (test) " << endl;
-    Sortlist* s = new Sortlist (a);
-    s->sortList();
+	// a is set as the head of List a
+	aList = new ListOfNumbers(a);
+	cout << "List a: " << endl;
+	aList -> PrintList();
 
-    // Displays the sorted list
-    cout << "The Sorted List " << endl;
-    s->PrintList();
+	cout << "Test the Add Method: " << endl;
+	aList->Add(new Node(45));
+	aList->Add(new Node(4));
+	aList->Add(new Node(80));
+	aList->Add(new Node(245));
+	aList -> PrintList();
+
+	cout << "Test the find Method: Find 80" << endl;
+	Node* found = aList->Find(80);
+	if (found != NULL) 
+    {
+		cout << "Node Found: " ;
+		found->PrintItem();
+	}
+	else
+    {
+		cout << "Node not found: " << endl;
+    }
+
+	cout << "Test the findR Method: Find 80" << endl;
+	Node* foundR = aList->FindR(80);
+	if (foundR != NULL) 
+    {
+		cout << "Node Found: ";
+		foundR->PrintItem();
+	}
+	else
+    {
+		cout << "Node not found: " << endl;
+    }
+
+	cout << "Test the AddR Method " << endl;
+	aList -> AddR(new Node(1));
+	aList -> AddR(new Node(688));
+	aList -> PrintList();
+	
+	cout << "Test the Remove Method: Remove an Existing Number " << endl;
+	aList -> Remove(2);
+	aList -> PrintList();
+
+	cout << "Test the Remove Method: Remove a Number Not Existing  " << endl;
+	aList -> Remove(3);
+	aList -> PrintList();
+
+	cout << "Test the RemoveR Method: Remove an existing Number " << endl;
+	aList -> Remove(455);
+	aList -> PrintList();
+
+
+	cout << "Test the Sort Method: " << endl;
+	aList -> SortList();
+
+	cout << "Sorted List: " << endl;
+	aList -> PrintList();
 }
 
 // Gets a number
@@ -48,4 +89,4 @@ int GetNumber()
     cout << "Please enter a number\n"; // User inputs a umber (temp)
     cin >> temp;
     return temp;
-}
+};
